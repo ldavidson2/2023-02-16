4545dc42049ff85be6054be73bb46401240ed765
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 14:15:32 -0600

    added api and function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "test2API": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "test2Function": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
   "storage": {
     "test2DB": {
       "Arn": "string",
