b42c8b00b95a379f2520affae2ed9fee08581ea2
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 09:46:54 -0600

    added api

---
+++ amplify/backend/function/newFunction/src/index.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name="us-east-2",
+         aws_access_key_id="AKIAX3FHVXYSKFFYPGE7",
+         aws_secret_access_key= "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")
+table = dynamodb.Table('apiTestTable-staging')
+
+def handler(event, context):
+    response = table.query(
+    KeyConditionExpression=Key('PK').eq('COMP#0')
+    )
+
+    data = str(response['Items'])
+    print('received event:')
+    print(event)
+  
+    return data
